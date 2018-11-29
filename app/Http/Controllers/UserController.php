a1db95362975f405e2f4364fadd9387d70a1a23d
oldshiji <1655664358@qq.com>
2018-11-29 17:24:18 +0800

    add welcome use page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends HttpController
 {
     public function test()
     {
-        $this->response->end("hi,users");
+        $this->response->end("<div style='text-align:center;font-size:30px;margin:200px auto;'>Hello,Siwo!^_^<p>基于swoole4.2.x版本撸的简易渣渣框架</p></div>");
 
     }
 
