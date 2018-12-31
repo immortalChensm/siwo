ae3733a20100b37607a206e2b4c2594af8488f77
oldshiji <1655664358@qq.com>
2018-12-31 14:01:34 +0800

    siwo framework update

--- siwo/start.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1655664358@qq.com
- * Date: 2018/11/19
- * Time: 0:44
- */
-require 'vendor/autoload.php';
-(new \Siwo\Foundation\Application())->run();
